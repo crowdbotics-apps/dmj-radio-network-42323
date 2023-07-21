93e942e6e9102e3b94d96a873fc61f9c9c005457
Crowdbotics <team@crowdbotics.com>
2023-07-21 00:54:01 +0000

    Initial commit.

---
+++ backend/dmj_radio_network_42323/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dmj_radio_network_42323 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmj_radio_network_42323.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dmj-radio-network-42323",
+  url: "https://dmj-radio-network-42323.botics.co",
+  api: "https://dmj-radio-network-42323.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
